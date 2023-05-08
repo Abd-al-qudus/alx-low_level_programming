c73df9432cc1ea282eb130d23c60fa7ff0368ca6
Abd-al-qudus <jimohqudus86@gmail.com>
2023-05-08 21:46:36 +0100

    fixed modulo error for task 1

--- 0x01-variables_if_else_while/1-last_digit.c
+++ 0x01-variables_if_else_while/1-last_digit.c
@@ -15,16 +15,12 @@ int main(void)
 
 	srand(time(0));
 	n = rand() - RAND_MAX / 2;
-	if (n < 0)
-		new = n * -1;
-	last = new % 10;
-	if (n < 0)
-		last = last * -1;
+	last = n % 10;
 	if (last == 0)
 		printf("Last digit of %d is %d and is 0\n", n, last);
 	else if (last > 5)
 		printf("Last digit of %d is %d and is greater than 5\n", n, last);
-	else
+	else if (last < 6)
 		printf("Last digit of %d is %d and is less than 6 and not 0\n", n, last);
 	return (0);
 }
