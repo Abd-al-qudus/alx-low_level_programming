41e4a767fc7d09bd4194d6ca9445a9dded0fa381
Abd-al-qudus <Abd-al-qudus>
2023-06-12 22:40:23 +0000

    solved pointers

--- 0x05-pointers_arrays_strings/7-puts_half.c
+++ 0x05-pointers_arrays_strings/7-puts_half.c
@@ -25,7 +25,7 @@ void puts_half(char *str)
 {
 	int n;
 
-	for (n = _strlen(str) / 2; n < _strlen(str); n++)
+	for (n = (_strlen(str) - 1) / 2; n < _strlen(str); n++)
 		_putchar(*(str + n));
 	_putchar('\n');
 }
--- 0x05-pointers_arrays_strings/8-print_array.c
+++ 0x05-pointers_arrays_strings/8-print_array.c
@@ -11,7 +11,7 @@ void print_array(int *a, int n)
 {
 	int i;
 
-	if (n >= 0)
+	if (n > 0)
 	{
 		for (i = 0; i < n; i++)
 			i < n - 1 ? printf("%d, ", *(a + i)) : printf("%d\n", *(a + i));
